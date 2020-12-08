f8658fc84f524f3e0be7dac26aeb803eb971ae5b
nikkhilkharade <nikhil.kharade@livehealth.in>
2020-12-08 22:25:47 +0530

    Created the base

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+What final out put should look like
+$​ create_parking_lot 6
+Created a parking lot with 6 slots
+$​ park KA-01-HH-1234 White Allocated slot number: 1
+$​ park KA-01-HH-9999 White Allocated slot number: 2
+$​ park KA-01-BB-0001 Black Allocated slot number: 3$
+​ park KA-01-HH-7777 Red Allocated slot number: 4
+$​ park KA-01-HH-2701 Blue Allocated slot number: 5
+$​ park KA-01-HH-3141 Black Allocated slot number: 6
+$​ leave 4
+Slot number 4 is free
+'''
+
+def leave():
+    pass
+
+
+def park():
+    pass
